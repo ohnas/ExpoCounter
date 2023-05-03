0adf78c31dc0d8565ce45d935aed9d29b67c48bb
ohnas <ohnas12@gmail.com>
2023-05-03 12:30:51 +0900

    1. counter,couterText,plus,plusTxt,miuns,miunsText

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.counter}>
+        <Text style={styles.counterText}>0</Text>
+      </View>
+      <View>
+        <Pressable style={styles.plus}>
+          <Text style={styles.plusText}>+</Text>
+        </Pressable>
+        <Pressable style={styles.miuns}>
+          <Text style={styles.miunsText}>-</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +26,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  counter: {
+    width: 300,
+    height: 100,
+    marginBottom: 50,
+  },
+  counterText: {
+    fontSize: 100,
+    textAlign: 'center',
+  },
+  plus: {
+    width: 350,
+    height: 400,
+    borderWidth: 2,
+    marginBottom: 30,
+    backgroundColor: 'gray',
+  },
+  plusText: {
+    fontSize: 200,
+    textAlign: 'center',
+  },
+  miuns: {
+    width: 350,
+    borderWidth: 2,
+    backgroundColor: 'gray',
+  },
+  miunsText: {
+    fontSize: 50,
+    textAlign: 'center',
   },
 });
