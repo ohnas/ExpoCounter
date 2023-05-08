f5d3eb011c8a0c2c2ff7f75454a49fccb5fff5a4
ohnas <ohnas12@gmail.com>
2023-05-08 18:43:15 +0900

    3. noGoal && goal

--- App.js
+++ App.js
@@ -3,8 +3,19 @@ import { useState } from 'react';
 import { Alert, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [header, setHeader] = useState("noGoal");
   const [goal, setGoal] = useState("");
   const [now, setNow] = useState(0);
+  function handleHeaderNoGoal() {
+    if(header === 'goal') {
+      setHeader('noGoal');
+    }
+  }
+  function handleHeaderGoal() {
+    if(header === 'noGoal') {
+      setHeader('goal');
+    }
+  }
   function handleGoal(value) {
     const numericValue = Number(value);
     const maxNumber = 100;
@@ -18,13 +29,16 @@ export default function App() {
     const stringValue = String(numericValue);
     setGoal(stringValue);
   }
-  function handlePlusPress() {
+  function handleGoalPlusPress() {
     setNow((previous) => previous + 1);
     if(now === Number(goal)) {
       Alert.alert("알림", "🎉 축하합니다");
       setNow(0);
       return;
     }
+  }
+  function handleNoGoalPlusPress() {
+    setNow((previous) => previous + 1);
   }
   function handleMinusPress() {
     setNow((previous) => previous - 1);
@@ -35,19 +49,45 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <View style={styles.counter}>
-        <Text style={styles.counterText}>{now}</Text>
-        <Text style={styles.goalText}>/</Text>
-        <TextInput style={styles.goalText} inputMode='numeric' returnKeyType='done' onChangeText={handleGoal} value={goal} placeholder='목표' />
+      <View style={styles.header}>
+        <Pressable onPress={handleHeaderNoGoal}>
+          <Text style={{...styles.headerText, color: header === 'noGoal' ? "black" : "gray"}}>노 목표</Text>
+        </Pressable>
+        <Pressable onPress={handleHeaderGoal}>
+          <Text style={{...styles.headerText, color: header === 'goal' ? "black" : "gray"}}>목표</Text>
+        </Pressable>
       </View>
-      <View>
-        <TouchableOpacity style={styles.plus} onPress={handlePlusPress}>
-          <Text style={styles.plusText}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.minus} onPress={handleMinusPress}>
-          <Text style={styles.minusText}>-</Text>
-        </TouchableOpacity>
-      </View>
+      {header === "noGoal" ? 
+        <>
+          <View style={styles.counter}>
+            <Text style={styles.counterText}>{now}</Text>
+          </View>
+          <View>
+            <TouchableOpacity style={styles.plus} onPress={handleNoGoalPlusPress}>
+              <Text style={styles.plusText}>+</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.minus} onPress={handleMinusPress}>
+              <Text style={styles.minusText}>-</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+        :
+        <>
+          <View style={styles.counter}>
+            <Text style={styles.counterText}>{now}</Text>
+            <Text style={styles.goalText}>/</Text>
+            <TextInput style={styles.goalText} inputMode='numeric' returnKeyType='done' onChangeText={handleGoal} value={goal} placeholder='목표' />
+          </View>
+          <View>
+            <TouchableOpacity style={styles.plus} onPress={handleGoalPlusPress}>
+              <Text style={styles.plusText}>+</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.minus} onPress={handleMinusPress}>
+              <Text style={styles.minusText}>-</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      }
       <StatusBar style="auto" />
     </View>
   );
@@ -59,6 +99,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header : {
+    width: 350,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  headerText: {
+    fontSize: 30,
   },
   counter: {
     width: 350,
@@ -75,7 +124,6 @@ const styles = StyleSheet.create({
   plus: {
     width: 350,
     height: 450,
-    // borderWidth: 2,
     borderRadius: 20,
     marginBottom: 30,
     backgroundColor: '#e1f7d5',
@@ -86,7 +134,6 @@ const styles = StyleSheet.create({
   },
   minus: {
     width: 350,
-    // borderWidth: 2,
     borderRadius: 20,
     backgroundColor: '#e1f7d5',
   },
